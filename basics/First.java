1899a8b0e6f13c3e8dd6efebc3ff386e81415f8c
shruti-19m <shrutimandal97293512@gmail.com>
2026-09-23 11:35:32 +0530

    updated

---
+++ basics/First.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class First {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("I love shruti very muchhh ....................");
+        System.out.println("My name is velora ....");
+
+        int roll = 57;
+        System.out.println("roll is" + roll);
+    }
+}
