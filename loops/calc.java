b197062b3a0dda8bb7366e179d7818ff72dddd5c
shruti-19m <shrutimandal97293512@gmail.com>
2026-09-23 20:07:05 +0530

    renamed file

--- loops/calc.java
+++ loops/calc.java
@@ -3,7 +3,7 @@
 package loops;
 import java.util.Scanner;
 
-public class problem {
+public class calc {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
     
