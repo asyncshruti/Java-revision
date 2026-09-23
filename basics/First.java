08c1adbe5673af2da5625f1df52717e24b42c383
shruti-19m <shrutimandal97293512@gmail.com>
2026-09-23 17:31:07 +0530

    added main file

--- basics/First.java
+++ basics/First.java
@@ -1,13 +1,11 @@
 package basics;
 
 public class First {
-    
     public static void main(String[] args) {
-        
-        System.out.println("I love shruti very muchhh ....................");
-        System.out.println("My name is velora ....");
-
-        int roll = 57;
-        System.out.println("roll is" + roll);
+        System.out.println("Hey! I'm velora ");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
     }
 }
---
+++ basics/Main.java
@@ -0,0 +1,42 @@
+package basics;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //variables
+        /* 
+        int a = 25;
+        int b = 10;
+        int sum = a + b;
+        int diff = a - b;
+        System.out.println(sum);
+        System.out.println(diff);
+
+        int mul = a * b;
+        System.out.println(mul);
+
+        //problem1:
+        
+        int a = 10;
+        int b = 5;
+        int ans = (a * b)/(a - b);
+        System.out.println(ans);
+
+        
+        // Input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
+
+        */
+        //problem2
+
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+       
+    }
+
+}
