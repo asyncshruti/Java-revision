228acb9a8c14e981003488fa8cdfdfdbe7a09321
shruti-19m <shrutimandal97293512@gmail.com>
2026-09-23 19:57:19 +0530

    changes on loop file

--- loops/loop.java
+++ loops/loop.java
@@ -2,7 +2,34 @@ package loops;
 
 public class loop {
     public static void main(String[] args) {
+        /* 
+        // ----for loop----
+
+        //counter++ => counter = counter + 1
+        for (int counter = 0; counter < 10; counter++) {
+           System.out.println("Hey! I'm Velora....");
+        }
         
+        //--print the number from 0 to 10
+        for(int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+            */
+        /* 
+        //---while loop---
+        int i = 0;
+        while(i <= 10) {
+            System.out.println(i);
+        }
+
+        */
+
+        //--Do while loop---
+        int i =0;
+        do {
+            System.out.println(i);
+            i = i+ 1;
+        } while(i < 11);
     }
-    
+
 }
